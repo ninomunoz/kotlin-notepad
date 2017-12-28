e94e6b507b7eaf0e499f279cc6a07b48bd36b2d4
Nino Munoz <mfmunoz@verisk.com>
2017-12-28 13:39:49 -0700

    refactor: convert NotesAdapter to Kotlin; add extension method for layout inflater

---
+++ app/src/main/java/com/udacity/notepad/util/_LayoutInflater.kt
@@ -0,0 +1,10 @@
+package com.udacity.notepad.util
+
+import android.content.Context
+import android.view.LayoutInflater
+
+/**
+ * Created by i57198 on 12/28/17.
+ */
+
+val Context.layoutInflater get() = LayoutInflater.from(this)
